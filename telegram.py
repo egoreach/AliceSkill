741b3ddb3805e8156e16bc26045a9dbe0686feea
egoreach <egorka.sold@gmail.com>
2022-04-21 21:30:01 +0700

    Рефакторинг + файл сессии

--- telegram.py
+++ telegram.py
@@ -1,57 +1,18 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl.types import PeerChannel
-from telethon import functions
-from telethon.tl.functions.messages import ImportChatInviteRequest
 
 from time import sleep
-from threading import Thread
-from warnings import filterwarnings
-
 
 from telegram_config import api_id, api_hash
 from google_sheets import add_post, is_in_channel_list, get_all_channels
-
-# Чтобы не засорять консоль
-# filterwarnings("ignore", category=RuntimeWarning)
-
-
-
-cached_channels = set()
-def subscribing():
-    global cached_channels
-
-    def subscribe_channel(channel):
-        try:
-            if '@' not in channel and '//' in channel:
-                async_to_sync(ImportChatInviteRequest(channel[channel.index('+') + 1:])())()
-                cached_channels.add(channel)
-            else:
-                async_to_sync(functions.channels.JoinChannelRequest(channel)())()
-                cached_channels.add(channel)
-        except: pass
-
-    for _ in range(10**11):
-        print(_)
-        now = set(get_all_channels())
-        to_subscribe = now - cached_channels
-        for channel in to_subscribe:
-            subscribe_channel(channel)
-
-        sleep(10)
-
-subscribing()
-daemon = Thread(target=subscribing())
-daemon.start()
 
 
 client = TelegramClient('telethon', api_id, api_hash)
 
 
-
 # Только новые сообщения
 @client.on(events.NewMessage())
 async def new_messages_handler(event):
-    await client(ImportChatInviteRequest("jgqJYN57NDdlYmNi"))
     try:
         raw_message = event.message.to_dict()  # сырое сообщение
         channel_id = raw_message['peer_id']['channel_id']
@@ -62,7 +23,7 @@ async def new_messages_handler(event):
         channel_username = channel_entity['username']
         channel_id = channel_entity['id']
 
-        # if is_in_channel_list(channel_username) or is_in_channel_list(channel_id):  # так ли нужно эта проверка (ведь
+        # if is_in_channel_list(channel_username) or is_in_channel_list(channel_id):  # так ли нужно эта проверка (ведь клиент подписан только на то, что нужно, а сообщения обрабатываются только из каналов)
         add_post(raw_message['message'], "@" + channel_username if isinstance(channel_username, str) else None, f'https://t.me/{channel_username}' if channel_username else None, channel_id)
 
     # Сообщания не от каналов не имеют поля channel_id
@@ -72,8 +33,3 @@ async def new_messages_handler(event):
 
 client.start()
 client.run_until_disconnected()
-
-
-# Подключение к каналу
-# await client(ImportChatInviteRequest('jgqJYN57NDdlYmNi'))
-# client(JoinChannelRequest("@kruginapole"))
