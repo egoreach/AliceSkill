384606ba0d631027ccce6745ee0752a99042c16f
egoreach <egorka.sold@gmail.com>
2022-05-02 16:54:54 +0700

    erqerb

--- subscribing.py
+++ subscribing.py
@@ -76,7 +76,7 @@ def main():
                 except Exception as e:
                     print(e, raw_channel)
                     cached_channels.add(raw_channel)
-
+        cached_channels.add(raw_channel)
 
 if __name__ == "__main__":
     main()
