d79ad5af742e2c0c525d4a7e1760ffc5b5d691ef
egoreach <egorka.sold@gmail.com>
2022-05-06 18:23:43 +0700

    ускорил

--- google_sheets.py
+++ google_sheets.py
@@ -1,9 +1,7 @@
 import gspread
 from time import sleep
 
-
 TABLE = "Database"
-
 
 first_account = gspread.service_account("first.json")
 first_table = first_account.open(TABLE)
@@ -11,13 +9,11 @@ first_table = first_account.open(TABLE)
 first_posts = first_table.worksheet("posts")
 first_channels = first_table.worksheet("channels")
 
-
 second_account = gspread.service_account("second.json")
 second_table = second_account.open(TABLE)
 
 second_posts = second_table.worksheet("posts")
 second_channels = second_table.worksheet("channels")
-
 
 third_account = gspread.service_account("third.json")
 third_table = third_account.open(TABLE)
@@ -25,15 +21,40 @@ third_table = third_account.open(TABLE)
 third_posts = third_table.worksheet("posts")
 third_channels = third_table.worksheet("channels")
 
-
 fourth_account = gspread.service_account("fourth.json")
 fourth_table = fourth_account.open(TABLE)
 
 fourth_posts = fourth_table.worksheet("posts")
 fourth_channels = fourth_table.worksheet("channels")
 
-
 channels_list = [first_channels, second_channels, third_channels, fourth_channels]
+channel_cnt = 0
+
+
+def get_idle_channels_account():
+    global channel_cnt
+
+    channel_cnt += 1
+    channel_cnt %= 4
+
+    sleep(0.26)
+
+    return channels_list[channel_cnt]
+
+
+posts_list = [first_posts, second_posts, third_posts, fourth_posts]
+posts_cnt = 0
+
+
+def get_idle_post_account():
+    global posts_cnt
+
+    posts_cnt += 1
+    posts_cnt %= 4
+
+    sleep(0.26)
+
+    return posts_list[posts_cnt]
 
 
 with open("current.txt", "w") as f:
@@ -42,8 +63,7 @@ with open("current.txt", "w") as f:
 
 def add_post(post, channel, channel_link, channel_id, title, date, unix) -> None:
     try:
-        sleep(1)
-        first_posts.append_row([post, channel, channel_link, channel_id, title, date, unix])
+        get_idle_post_account().append_row([post, channel, channel_link, channel_id, title, date, unix])
 
         with open("current.txt", "r+") as f:
             current_post_number = int(f.read())
@@ -51,11 +71,14 @@ def add_post(post, channel, channel_link, channel_id, title, date, unix) -> None
         with open("current.txt", "w") as f:
             f.write(str(current_post_number))
 
+        cell = get_idle_channels_account().find(channel)
+        value = get_idle_channels_account().cell(cell.row, cell.col + 1).value
 
-        cell = second_channels.find(channel)
-        value = third_channels.cell(cell.row, cell.col + 1).value
+        print(value + "," + str(current_post_number) if value else str(current_post_number))
 
-        fourth_channels.update_cell(cell.row, cell.col + 1, value + "," + str(current_post_number) if value else str(current_post_number))
+        get_idle_channels_account().update_cell(cell.row, cell.col + 1,
+                                                value + "," + str(current_post_number) if value else str(
+                                                    current_post_number))
     except gspread.exceptions.APIError:
         print("gspread.exceptions.APIError")
         sleep(31)
@@ -65,16 +88,15 @@ def add_post(post, channel, channel_link, channel_id, title, date, unix) -> None
 
 def add_posts(some_posts):
     try:
-        sleep(1)
-        first_posts.append_rows(some_posts)
+        get_idle_post_account().append_rows(some_posts)
         with open("current.txt", "r+") as f:
             current_post_number = int(f.read())
             current_post_number += len(some_posts)
         with open("current.txt", "w") as f:
             f.write(str(current_post_number))
 
-        cell = second_channels.find(some_posts[0][1])
-        value = third_channels.cell(cell.row, cell.col + 1).value
+        cell = get_idle_channels_account().find(some_posts[0][1])
+        value = get_idle_channels_account().cell(cell.row, cell.col + 1).value
 
         new = ",".join([str(i) for i in range(current_post_number - len(some_posts) + 1, current_post_number + 1)])
         if value:
@@ -82,29 +104,27 @@ def add_posts(some_posts):
         else:
             new_value = new
 
-        fourth_channels.update_cell(cell.row, cell.col + 1, new_value)
+        get_idle_channels_account().update_cell(cell.row, cell.col + 1, new_value)
     except gspread.exceptions.APIError:
         print("gspread.exceptions.APIError")
         sleep(31)
     except Exception as e:
         print(e)
-
 
 
 def get_all_channels() -> set:
     try:
-        sleep(1)
-        return set(second_channels.col_values(1))
+        return set(get_idle_channels_account().col_values(1))
     except gspread.exceptions.APIError:
         print("gspread.exceptions.APIError")
         sleep(31)
     except Exception as e:
         print(e)
 
+
 def add_channel(channel):
     try:
-        sleep(1)
-        fourth_channels.append_row([channel])
+        get_idle_channels_account().append_row([channel])
     except gspread.exceptions.APIError:
         print("gspread.exceptions.APIError")
         sleep(31)
